6834db0e6564ca012c392e4a4d1b49bfc522d697
Alice <ayz7bs@virginia.edu>
2015-04-09 23:21:28 -0400

    chocolate chip cookie gives grandma speed boost and disappears

--- MowingforCookies/MainGame.cs
+++ MowingforCookies/MainGame.cs
@@ -130,11 +130,8 @@ namespace MowingforCookies
 
             }
 
-            // can convert this into Tiled stuff later
-            Cookie c1 = new Cookie(patches[4,0],4, 0);
-            cookies.Add(c1);
-            
-
+            Cookie c2 = new Cookie(patches[5, 0], 5, 0);
+            cookies.Add(c2);
 
             base.Initialize();
             controls = new Controls();
@@ -232,6 +229,10 @@ namespace MowingforCookies
                 
                 o.Update(patches,mower,enemies,ticks);
             }
+            foreach (Cookie c in cookies)
+            {
+                c.Update(mower, grandma, ticks);
+            }
         }
 
         /// <summary>
@@ -253,7 +254,10 @@ namespace MowingforCookies
             }
             foreach (Cookie c in cookies)
             {
-                c.Draw(spriteBatch);
+                if (c.alive)
+                {
+                    c.Draw(spriteBatch);
+                }
             }
             mower.Draw(spriteBatch);
             grandma.Draw(spriteBatch);
